e04a0e0d9ac8b33cc9a5a1a9eae879b9791bad6b
Begana Choi <begana.contact@gmail.com>
2024-11-05 17:45:09 +0100

    create CardItem

---
+++ src/components/CardItem.jsx
@@ -0,0 +1,16 @@
+export default function CardItem({ card }) {
+  return (
+    <li className="card-item">
+      <img src={card.images.small} alt={card.name} />
+      <div>
+        <h3>{card.name}</h3>
+        <p className="card-item-price">
+          {card.cardmarket.prices.averageSellPrice}
+        </p>
+      </div>
+      <p className="card-item-actions">
+        <button>Add To Cart</button>
+      </p>
+    </li>
+  );
+}
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,8 @@
 import { useState, useEffect } from "react";
 
-const API_KEY = "41d2dff6-e2ac-4365-ba97-8c792f3cd280";
+import CardItem from "./CardItem";
 
-{
-  /* <ul id="cards">
-      {loadedCards.map((card) => (
-        <li key={card.id}>
-          <img src={card.images.small} alt="" />
-          <p>{card.tcgplayer.prices.market}</p>
-        </li>
-      ))}
-    </ul> */
-}
+const API_KEY = "41d2dff6-e2ac-4365-ba97-8c792f3cd280";
 
 export default function Cards() {
   const [loadedCards, setLoadedCards] = useState();
@@ -19,7 +10,11 @@ export default function Cards() {
   useEffect(() => {
     try {
       async function fetchCards() {
-        const response = await fetch("https://api.pokemontcg.io/v2/cards");
+        const response = await fetch("https://api.pokemontcg.io/v2/cards", {
+          headers: {
+            "X-Api-Key": API_KEY,
+          },
+        });
         if (!response.ok) {
           //...
         }
@@ -37,23 +32,9 @@ export default function Cards() {
   return (
     <ul id="cards">
       {loadedCards &&
-        loadedCards.slice(0, 20).map((card) => (
-          <li key={card.id} className="card-item">
-            <img src={card.images.small} alt="" />
-            <div>
-              <h3>{card.name}</h3>
-              <p className="card-item-price">
-                {card.cardmarket.prices.averageSellPrice}
-              </p>
-              <p className="card-item-description">
-                level: {card.level} hp: {card.hp} types: {card.types[0]}
-              </p>
-            </div>
-            <p className="card-item-actions">
-              <button>Add To Cart</button>
-            </p>
-          </li>
-        ))}
+        loadedCards
+          .slice(0, 21)
+          .map((card) => <CardItem key={card.id} card={card} />)}
     </ul>
   );
 }
