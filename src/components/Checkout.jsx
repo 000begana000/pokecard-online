58a82a07efc02fe42785f83e04912da42b990385
Begana Choi <begana.contact@gmail.com>
2024-11-07 16:10:36 +0100

    feat: add Input field in Checkout modal

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,8 +1,12 @@
 import { useContext } from "react";
 
+// context
 import CartContext from "../store/CartContext";
 
+// components
 import Modal from "./UI/Modal.jsx";
+import Input from "./UI/Input.jsx";
+import Button from "./UI/Button.jsx";
 
 export default function Checkout() {
   const cartCtx = useContext(CartContext);
@@ -18,6 +22,17 @@ export default function Checkout() {
       <form action="">
         <h2>Checkout</h2>
         <p>Total Price: {cartTotal}</p>
+        <Input label="Full Name" type="text" id="name" />
+        <Input label="Email Address" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal Code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+        <p className="modal-actions">
+          <Button type="button">Close</Button>
+          <Button>Submit Order</Button>
+        </p>
       </form>
     </Modal>
   );
